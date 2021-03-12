68b295e873824722998d745759513244b8c4500c
luwenjing <luwenjing@didichuxing.com>
2021-03-12 17:49:00 +0800

    change readme & remove Access-Control-Allow-Credentials

--- tips-web/app.js
+++ tips-web/app.js
@@ -9,7 +9,6 @@ const config = require('./config');
 const app = express();
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods','PUT,POST,GET,DELETE,OPTIONS');
   res.header('Content-Type', 'application/json;charset=utf-8');
